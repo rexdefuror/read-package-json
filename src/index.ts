1f9ff3aa13d3b6357cf73c0c0934663156912148
rexdefuror <srdjan.debic@gmail.com>
2022-11-30 16:07:36 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import util from 'util';
+import { join } from 'path';
+import { getInput, exportVariable, setFailed } from '@actions/core';
+
+const main = async () => {
+    try {
+        const path = getInput('path');
+        const fileName = getInput('file');
+
+        const file = await fs.promises.readFile(join(path, fileName), 'utf8');
+
+        if (!file) {
+            setFailed('Could not read file');
+            return;
+        }
+        const packageFile = JSON.parse(file);
+        if (!packageFile) {
+            setFailed('Could not parse package.json');
+            return;
+        }
+
+        const justFileName = fileName.split('.')[0];
+
+        exportVariable(`${justFileName}_NAME`, packageFile.name);
+        exportVariable(`${justFileName.toUpperCase()}_VERSION`, packageFile.version);
+    }
+    catch (error: any) {
+        setFailed(error.message);
+    }
+}
+
+main();
+
